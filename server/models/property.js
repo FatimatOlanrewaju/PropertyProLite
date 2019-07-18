fd54d4f06d32019c0ee2ecf5677d5e8c17e06db2
Fatimat Olanrewaju <fatimat_olanrewaju@yahoo.com>
2019-07-18 08:59:40 +0100

    feature(updatepropertyEndpoint): user can update property advert [#167329137]

--- server/models/property.js
+++ server/models/property.js
@@ -3,21 +3,22 @@ import propertyData from '../data/property.data';
 
 
 class Properties{
-    postProperty(property) {
-        const newProperty = {
-        id:propertyData.length + 1,
-        owner: users.length - 1,
-        email: users.length - 1,
-        price: parseFloat(property.price),
-        state: property.state,
-        address: property.address,
-        city: property.city,
-        type: property.type,
-        image_url: property.image_url,
-        created_on: new Date(),
-        }
-        propertyData.push(newProperty);
-        return newProperty;
+  postProperty(property) {
+    const newProperty = {
+      id:propertyData.length + 1,
+      owner: users.length - 1,
+      email: users.length - 1,
+      price: parseFloat(property.price),
+      state: property.state,
+      address: property.address,
+      city: property.city,
+      type: property.type,
+      image_url: property.image_url,
+      created_on: new Date(),
     }
+   
+    propertyData.push(newProperty);
+    return newProperty;
+  }
 }
 export default new Properties();
